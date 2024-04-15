78d15cc36e0d33c30d4ac59041d669b6faa9cedd
Mgokayay <aymustafagokay@gmail.com>
2024-04-15 18:13:09 +0300

    product entity created

---
+++ src/main/java/com/workintech/backend/entity/Product.java
@@ -0,0 +1,42 @@
+package com.workintech.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "product",schema = "ecommercebackend")
+public class Product {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private Double price;
+
+    @Column(name = "sell_count")
+    private Long sellCount;
+
+    @Column(name = "stock")
+    private Long stock;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "images")
+    private String images;
+
+    @ManyToOne(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+}
