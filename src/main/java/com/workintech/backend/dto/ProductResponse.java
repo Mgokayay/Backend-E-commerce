25cbd247cecf12a95b5d5ed99f3b5442bb746e80
Mgokayay <aymustafagokay@gmail.com>
2024-04-15 20:06:37 +0300

    product response created

---
+++ src/main/java/com/workintech/backend/dto/ProductResponse.java
@@ -0,0 +1,4 @@
+package com.workintech.backend.dto;
+
+public record ProductResponse(Long id,String name,Double price,Long sellCount,Long stock,String description,String images,Long categoryId) {
+}
