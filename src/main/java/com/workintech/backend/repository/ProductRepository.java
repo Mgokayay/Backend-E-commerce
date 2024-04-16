5bbc22a1f374ca64a2bc3007324aec427c154253
Mgokayay <aymustafagokay@gmail.com>
2024-04-16 19:17:24 +0300

    query added to repository

--- src/main/java/com/workintech/backend/repository/ProductRepository.java
+++ src/main/java/com/workintech/backend/repository/ProductRepository.java
@@ -2,6 +2,12 @@ package com.workintech.backend.repository;
 
 import com.workintech.backend.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product,Long> {
+
+    @Query("SELECT p FROM Product p ORDER BY p.price ASC")
+    List<Product> sortByPriceAsc();
 }
