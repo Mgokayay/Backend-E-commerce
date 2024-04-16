1e9e883da1e2f7cb322b5da57fae4efee1539ae0
Mgokayay <aymustafagokay@gmail.com>
2024-04-16 19:18:57 +0300

    exception handler to the global exception handler

--- src/main/java/com/workintech/backend/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/workintech/backend/exceptions/GlobalExceptionHandler.java
@@ -4,10 +4,15 @@ package com.workintech.backend.exceptions;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 @ControllerAdvice
@@ -22,6 +27,17 @@ public class GlobalExceptionHandler {
                 LocalDateTime.now());
 
         return new ResponseEntity<>(categoryErrorResponse,categoryException.getHttpStatus());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity handleException (MethodArgumentNotValidException exception){
+        List errorList = exception.getBindingResult().getFieldErrors().stream()
+                .map(fieldError -> {
+                    Map<String, String> errorMap = new HashMap<>();
+                    errorMap.put(fieldError.getField(),fieldError.getDefaultMessage());
+                    return errorMap;
+                }).collect(Collectors.toList());
+        return new ResponseEntity(errorList , HttpStatus.BAD_REQUEST);
     }
 
     public ResponseEntity<CategoryErrorResponse> handleException(Exception exception){
