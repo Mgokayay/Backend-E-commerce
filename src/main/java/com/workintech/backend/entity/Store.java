2516f0c5d45668bfb8603dbeb0517069ace16afc
Mgokayay <aymustafagokay@gmail.com>
2024-04-17 17:47:43 +0300

    store entity created

---
+++ src/main/java/com/workintech/backend/entity/Store.java
@@ -0,0 +1,50 @@
+package com.workintech.backend.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "store",schema = "ecommercebackend")
+public class Store {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "phone")
+    private Long phone;
+
+    @Column(name = "tax_no")
+    private Long taxNo;
+
+    @Column(name = "bank_account")
+    private Long bankAccount;
+
+
+
+    @JsonBackReference
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "store")
+    private List<Product> products;
+
+
+    public void addProduct(Product product){
+        if(products == null){
+            products = new ArrayList<>();
+        }
+        products.add(product);
+    }
+
+
+}
