c17bd56972f19289aabc867b8bb66ddf3a5b8110
Mgokayay <aymustafagokay@gmail.com>
2024-04-17 17:44:40 +0300

    some features added to product controller

--- src/main/java/com/workintech/backend/controller/ProductController.java
+++ src/main/java/com/workintech/backend/controller/ProductController.java
@@ -3,8 +3,10 @@ package com.workintech.backend.controller;
 import com.workintech.backend.dto.ProductResponse;
 import com.workintech.backend.entity.Category;
 import com.workintech.backend.entity.Product;
+import com.workintech.backend.entity.Store;
 import com.workintech.backend.service.CategoryService;
 import com.workintech.backend.service.ProductService;
+import com.workintech.backend.service.StoreService;
 import com.workintech.backend.util.ProductDtoConvertion;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -18,10 +20,13 @@ public class ProductController {
     private final ProductService productService;
 
     private final CategoryService categoryService;
+
+    private final StoreService storeService;
     @Autowired
-    public ProductController(ProductService productService, CategoryService categoryService) {
+    public ProductController(ProductService productService, CategoryService categoryService, StoreService storeService) {
         this.productService = productService;
         this.categoryService = categoryService;
+        this.storeService = storeService;
     }
     @GetMapping("/")
     public List<ProductResponse> findAll(){
@@ -37,13 +42,27 @@ public class ProductController {
         return productService.findAllByPriceAsc();
     }
 
-    @PostMapping("/{categoryId}")
-    public ProductResponse save(@RequestBody Product product,@PathVariable Long categoryId){
+    @GetMapping("/price:desc")
+    public List<ProductResponse> findAllByPriceDesc(){
+        return productService.findAllByPriceDesc();
+    }
+    @GetMapping("/param")
+    public List<ProductResponse> filterByName(@RequestParam String search){
+        return productService.filterByName(search);
+    }
+
+    @PostMapping("/{categoryId}/{storeId}")
+    public ProductResponse save(@RequestBody Product product,@PathVariable Long categoryId,@PathVariable Long storeId){
         Category category = categoryService.findByCategoryId(categoryId);
+        Store store = storeService.findByStoreId(storeId);
         product.setCategory(category);
-        category.addProduct(product);
-        categoryService.save(category);
+        product.setStore(store);
+
+
+
         productService.save(product);
+        category.addProduct(product);
+        store.addProduct(product);
 
 
         return ProductDtoConvertion.convertProduct(product);
