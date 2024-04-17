71e05bc27810cd436df389108ec05a3dda0fc78b
Mgokayay <aymustafagokay@gmail.com>
2024-04-17 17:43:11 +0300

    some little changes have been done

--- src/main/java/com/workintech/backend/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/workintech/backend/exceptions/GlobalExceptionHandler.java
@@ -18,11 +18,11 @@ import java.util.stream.Collectors;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(CategoryException.class)
-    public ResponseEntity<CategoryErrorResponse> handleException(CategoryException categoryException){
+    @ExceptionHandler(CommonException.class)
+    public ResponseEntity<CommonErrorResponse> handleException(CommonException categoryException){
         log.error("CategoryException occured ! Exception details : " + categoryException.getMessage());
 
-        CategoryErrorResponse categoryErrorResponse = new CategoryErrorResponse(categoryException.getHttpStatus().value(),
+        CommonErrorResponse categoryErrorResponse = new CommonErrorResponse(categoryException.getHttpStatus().value(),
                 categoryException.getMessage(),
                 LocalDateTime.now());
 
@@ -40,10 +40,10 @@ public class GlobalExceptionHandler {
         return new ResponseEntity(errorList , HttpStatus.BAD_REQUEST);
     }
 
-    public ResponseEntity<CategoryErrorResponse> handleException(Exception exception){
+    public ResponseEntity<CommonErrorResponse> handleException(Exception exception){
         log.error("GeneralException occured ! Exception details : " + exception.getMessage());
 
-        CategoryErrorResponse categoryErrorResponse = new CategoryErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+        CommonErrorResponse categoryErrorResponse = new CommonErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(),
                 exception.getMessage(),LocalDateTime.now());
 
         return new ResponseEntity<>(categoryErrorResponse , HttpStatus.INTERNAL_SERVER_ERROR);
