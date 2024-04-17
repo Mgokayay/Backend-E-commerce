068f3ab0d11c624d8871c6494a75bdbaebd07f7c
Mgokayay <aymustafagokay@gmail.com>
2024-04-17 17:48:11 +0300

    store dto convertion created

---
+++ src/main/java/com/workintech/backend/util/StoreDtoConvertion.java
@@ -0,0 +1,23 @@
+package com.workintech.backend.util;
+
+import com.workintech.backend.dto.StoreResponse;
+import com.workintech.backend.entity.Store;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StoreDtoConvertion {
+
+    public static List<StoreResponse> convertStoreList(List<Store> stores){
+        List<StoreResponse> storeResponses = new ArrayList<>();
+
+        stores.stream().forEach(store -> storeResponses.add(new StoreResponse(store.getId(),store.getName()
+                ,store.getPhone(),store.getTaxNo(),store.getBankAccount(),store.getProducts())));
+        return storeResponses;
+    }
+
+    public static StoreResponse convertStore(Store store){
+        return new StoreResponse(store.getId(),store.getName()
+                ,store.getPhone(),store.getTaxNo(),store.getBankAccount(),store.getProducts());
+    }
+}
