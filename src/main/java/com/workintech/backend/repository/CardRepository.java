50cf0f27a2736ed7260429000ebddc640afc761a
Mgokayay <aymustafagokay@gmail.com>
2024-04-18 19:48:13 +0300

    card repository created

---
+++ src/main/java/com/workintech/backend/repository/CardRepository.java
@@ -0,0 +1,7 @@
+package com.workintech.backend.repository;
+
+import com.workintech.backend.entity.Card;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CardRepository extends JpaRepository<Card,Long> {
+}
