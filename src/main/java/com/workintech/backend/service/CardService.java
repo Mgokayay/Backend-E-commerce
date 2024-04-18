14a5d544d02ed20f403e121728d2ff94c9d27d89
Mgokayay <aymustafagokay@gmail.com>
2024-04-18 19:48:38 +0300

    card service created

---
+++ src/main/java/com/workintech/backend/service/CardService.java
@@ -0,0 +1,17 @@
+package com.workintech.backend.service;
+
+import com.workintech.backend.dto.CardResponse;
+import com.workintech.backend.entity.Card;
+
+import java.util.List;
+
+public interface CardService {
+
+    List<CardResponse> findAll();
+
+    CardResponse findById(Long id);
+
+    CardResponse save(Card card);
+
+    CardResponse delete(Long id);
+}
