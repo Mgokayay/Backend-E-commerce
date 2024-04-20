8817b794be1159d0d327c9761504f2c0b5af5b9f
Mgokayay <aymustafagokay@gmail.com>
2024-04-20 19:12:40 +0300

    user entity created

---
+++ src/main/java/com/workintech/backend/entity/User.java
@@ -0,0 +1,46 @@
+package com.workintech.backend.entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "user",schema = "ecommercebackend")
+public class User{
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @JsonBackReference
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "user")
+    private List<Card> cards;
+
+    public void addCard(Card card){
+        if(cards == null){
+            cards = new ArrayList<>();
+        }
+        cards.add(card);
+    }
+
+
+
+}
