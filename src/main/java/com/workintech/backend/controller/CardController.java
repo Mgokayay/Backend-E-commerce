7a505a4297c9cec6b3b184b83a4e6909a762219e
Mgokayay <aymustafagokay@gmail.com>
2024-04-24 17:03:35 +0300

    userId added to the save method in cardcontroller

--- src/main/java/com/workintech/backend/controller/CardController.java
+++ src/main/java/com/workintech/backend/controller/CardController.java
@@ -2,31 +2,41 @@ package com.workintech.backend.controller;
 
 import com.workintech.backend.dto.CardResponse;
 import com.workintech.backend.entity.Card;
+import com.workintech.backend.entity.User;
 import com.workintech.backend.repository.CardRepository;
 import com.workintech.backend.service.CardService;
+import com.workintech.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/card")
 public class CardController {
 
     private final CardService cardService;
+    private final UserService userService;
     @Autowired
-    public CardController(CardService cardService) {
+    public CardController(CardService cardService, UserService userService) {
         this.cardService = cardService;
+        this.userService = userService;
     }
 
 
-    @PostMapping
+    @GetMapping
     public List<CardResponse> findAll(){
+
         return cardService.findAll();
     }
 
-    @GetMapping("/")
-    public CardResponse save(@RequestBody Card card){
+    @PostMapping("/{userId}")
+    public CardResponse save(@RequestBody Card card,@PathVariable Long userId){
+        User user = userService.findByUserId(userId);
+        user.getCards().add(card);
+        card.setUser(user);
+
         return cardService.save(card);
     }
 
