64952b1c6674a26c7ef90fac168716add7882828
Mgokayay <aymustafagokay@gmail.com>
2024-04-24 17:01:41 +0300

    address response created

---
+++ src/main/java/com/workintech/backend/dto/AddressResponse.java
@@ -0,0 +1,5 @@
+package com.workintech.backend.dto;
+
+public record AddressResponse(Long id,String city,String district,String name,String phone,String title,
+                              String neighborhood,String address) {
+}
