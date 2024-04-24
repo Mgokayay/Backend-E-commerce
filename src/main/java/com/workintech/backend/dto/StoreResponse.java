f4a724e12d6f73a8a26b915ce285d014acad9d5e
Mgokayay <aymustafagokay@gmail.com>
2024-04-24 17:05:43 +0300

    a parameter added to the Storeresponse

--- src/main/java/com/workintech/backend/dto/StoreResponse.java
+++ src/main/java/com/workintech/backend/dto/StoreResponse.java
@@ -4,5 +4,6 @@ import com.workintech.backend.entity.Product;
 
 import java.util.List;
 
-public record StoreResponse(Long id, String name, Long phone, Long tax_no, Long bank_account, List<Product> productList) {
+public record StoreResponse(Long id, String name, Long phone, Long tax_no, Long bank_account,
+                            List<Product> productList,Long userId) {
 }
